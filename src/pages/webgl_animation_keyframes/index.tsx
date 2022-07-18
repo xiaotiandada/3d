d6180b42f2405a6967884fed7852d1df558dbc9e
xiaotian <istianlei@qq.com>
2022-07-18 14:42:58 +0800

    feat: environment

--- src/pages/webgl_animation_keyframes/index.tsx
+++ src/pages/webgl_animation_keyframes/index.tsx
@@ -1,54 +1,59 @@
-import { Environment, OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import { FC, Suspense, useEffect, useRef } from 'react'
+import { Environment, OrbitControls, Stats } from '@react-three/drei'
+import { Canvas, useThree } from '@react-three/fiber'
+import { FC, Suspense } from 'react'
+import * as THREE from 'three'
+import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment'
 
 import Helper from '../../components/Helper'
 import Model from './components/Model'
 // https://threejs.org/examples/#webgl_animation_keyframes
 
 const Wrapper = () => {
+  const { gl } = useThree()
+
+  const pmremGenerator = new THREE.PMREMGenerator(gl)
+  const PMREMGenerator = pmremGenerator.fromScene(
+    new RoomEnvironment(),
+    0.04
+  ).texture
+
   return (
     <>
       <color attach="background" args={[0xbfe3dd]} />
       <Suspense fallback={null}>
         <Model scale={[0.01, 0.01, 0.01]} position={[1, 1, 0]} />
-        <Environment preset="sunset" background />
+        <Environment map={PMREMGenerator} />
       </Suspense>
     </>
   )
 }
 
 const Comic: FC = () => {
-  const modelRef = useRef()
-
-  useEffect(() => {
-    if (modelRef.current) {
-      //
-    }
-  }, [modelRef])
-
   return (
-    <Canvas
-      camera={{
-        position: [5, 2, 8],
-        fov: 40,
-        aspect: window.innerWidth / window.innerHeight,
-        near: 1,
-        far: 100,
-      }}
-    >
-      <Wrapper />
-      {process.env.NODE_ENV === 'development' && (
-        <group>
-          <Helper />
-        </group>
-      )}
-      <OrbitControls
-        target={[0, 0.5, 0]}
-        enableDamping={true}
-        enablePan={false}
-      />
-    </Canvas>
+    <>
+      <Canvas
+        camera={{
+          position: [5, 2, 8],
+          fov: 40,
+          aspect: window.innerWidth / window.innerHeight,
+          near: 1,
+          far: 100,
+        }}
+      >
+        <Wrapper />
+        {process.env.NODE_ENV === 'development' && (
+          <group>
+            <Helper />
+          </group>
+        )}
+        <OrbitControls
+          target={[0, 0.5, 0]}
+          enableDamping={true}
+          enablePan={false}
+        />
+      </Canvas>
+      {process.env.NODE_ENV === 'development' && <Stats className="fps" />}
+    </>
   )
 }
 
